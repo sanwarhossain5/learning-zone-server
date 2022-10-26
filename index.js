30a10512c0f92693f57bb0dd445f82413bb91236
sanwarhossain5 <sonymix5000@gmail.com>
2022-10-27 02:34:52 +0600

    data added

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors')
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const courses = require('./data/course.json');
+
+
+app.get('/' , (req , res) =>{
+  res.send(' Learning API is running')
+}); 
+
+app.get('/courses' , (req , res) =>{
+  res.send(courses)
+});
+
+app.get('/courses/:id' , (req , res) =>{
+
+  const id = parseInt(req.params.id);
+  const course = courses.find(course => course.id === id);
+  res.send(course)
+});
+
+
+app.listen(port , () =>{
+  console.log('Learning news server ' , port);
+});
